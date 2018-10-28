d3b48bd0119ff3e2d7b219975a1d5bfcdd37abd2
Joseph Watkin <joseph.watkin@gmail.com>
2018-10-28 16:35:32 -0400

    Removed old file

--- create-dbs.sh
+++
@@ -1,6 +0,0 @@
-ROOTPWD=generaladmin
-
-# evl "cat ${ROOTDIR}/mysql/create-db-and-users.sql | mysql -u root -p${ROOTPWD}"
-
-cat create-mysql-users.sql | mysql -u root -p$ROOTPWD
-cat wiki-mysql.sql | mysql -u root -p$ROOTPWD
