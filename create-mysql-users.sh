8a564382b4ec99021ec76a4f42f7202eb963104d
Joseph Watkin <joseph.watkin@gmail.com>
2018-10-28 19:36:16 -0400

    Fixed a bug where the username is too long

--- create-mysql-users.sh
+++ create-mysql-users.sh
@@ -13,31 +13,31 @@ echo "Done Root User Set up"
 
 echo "Users set up ... "
 mysql -uroot -p${ROOT_PASSWORD} <<_EOF_
-CREATE USER 'josephwatkinadmin'@'localhost' IDENTIFIED BY ${PR_PASSWORD};
-CREATE USER 'josephwatkinadmin_dev'@'localhost' IDENTIFIED BY ${DEV_PASSWORD};
-CREATE USER 'josephwatkinadmin_qa'@'localhost' IDENTIFIED BY ${QA_PASSWORD};
-CREATE USER 'josephwatkinadmin_ro'@'localhost' IDENTIFIED BY ${RO_PASSWORD};
-CREATE USER 'josephwatkinadmin'@'%' IDENTIFIED BY ${PR_PASSWORD};
-CREATE USER 'josephwatkinadmin_dev'@'%' IDENTIFIED BY ${DEV_PASSWORD};
-CREATE USER 'josephwatkinadmin_qa'@'%' IDENTIFIED BY ${QA_PASSWORD};
-CREATE USER 'josephwatkinadmin_ro'@'%' IDENTIFIED BY ${RO_PASSWORD};
+CREATE USER 'jwadmin'@'localhost' IDENTIFIED BY ${PR_PASSWORD};
+CREATE USER 'jwadmin_dev'@'localhost' IDENTIFIED BY ${DEV_PASSWORD};
+CREATE USER 'jwadmin_qa'@'localhost' IDENTIFIED BY ${QA_PASSWORD};
+CREATE USER 'jwadmin_ro'@'localhost' IDENTIFIED BY ${RO_PASSWORD};
+CREATE USER 'jwadmin'@'%' IDENTIFIED BY ${PR_PASSWORD};
+CREATE USER 'jwadmin_dev'@'%' IDENTIFIED BY ${DEV_PASSWORD};
+CREATE USER 'jwadmin_qa'@'%' IDENTIFIED BY ${QA_PASSWORD};
+CREATE USER 'jwadmin_ro'@'%' IDENTIFIED BY ${RO_PASSWORD};
 DROP DATABASE IF EXISTS josephwatkin_pr; CREATE DATABASE josephwatkin_pr CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL ON josephwatkin_pr.* TO 'josephwatkinadmin'@'localhost';
-GRANT ALL ON josephwatkin_pr.* TO 'josephwatkinadmin'@'%';
+GRANT ALL ON josephwatkin_pr.* TO 'jwadmin'@'localhost';
+GRANT ALL ON josephwatkin_pr.* TO 'jwadmin'@'%';
 DROP DATABASE IF EXISTS josephwatkin_dev; CREATE DATABASE josephwatkin_dev CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL ON josephwatkin_dev.* TO 'josephwatkinadmin'@'localhost';
-GRANT ALL ON josephwatkin_dev.* TO 'josephwatkinadmin'@'%';
-GRANT ALL ON josephwatkin_dev.* TO 'josephwatkinadmin_dev'@'localhost';
-GRANT ALL ON josephwatkin_dev.* TO 'josephwatkinadmin_dev'@'%';
-GRANT SELECT, SHOW VIEW ON josephwatkin_dev.* TO 'josephwatkinadmin_ro'@'localhost';
-GRANT SELECT, SHOW VIEW ON josephwatkin_dev.* TO 'josephwatkinadmin_ro'@'%';
+GRANT ALL ON josephwatkin_dev.* TO 'jwadmin'@'localhost';
+GRANT ALL ON josephwatkin_dev.* TO 'jwadmin'@'%';
+GRANT ALL ON josephwatkin_dev.* TO 'jwadmin_dev'@'localhost';
+GRANT ALL ON josephwatkin_dev.* TO 'jwadmin_dev'@'%';
+GRANT SELECT, SHOW VIEW ON josephwatkin_dev.* TO 'jwadmin_ro'@'localhost';
+GRANT SELECT, SHOW VIEW ON josephwatkin_dev.* TO 'jwadmin_ro'@'%';
 DROP DATABASE IF EXISTS josephwatkin_qa; CREATE DATABASE josephwatkin_qa CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL ON josephwatkin_qa.* TO 'josephwatkinadmin'@'localhost';
-GRANT ALL ON josephwatkin_qa.* TO 'josephwatkinadmin'@'%';
-GRANT ALL ON josephwatkin_qa.* TO 'josephwatkinadmin_dev'@'localhost';
-GRANT ALL ON josephwatkin_qa.* TO 'josephwatkinadmin_dev'@'%';
-GRANT SELECT, SHOW VIEW ON josephwatkin_qa.* TO 'josephwatkinadmin_ro'@'localhost';
-GRANT SELECT, SHOW VIEW ON josephwatkin_qa.* TO 'josephwatkinadmin_ro'@'%';
+GRANT ALL ON josephwatkin_qa.* TO 'jwadmin'@'localhost';
+GRANT ALL ON josephwatkin_qa.* TO 'jwadmin'@'%';
+GRANT ALL ON josephwatkin_qa.* TO 'jwadmin_dev'@'localhost';
+GRANT ALL ON josephwatkin_qa.* TO 'jwadmin_dev'@'%';
+GRANT SELECT, SHOW VIEW ON josephwatkin_qa.* TO 'jwadmin_ro'@'localhost';
+GRANT SELECT, SHOW VIEW ON josephwatkin_qa.* TO 'jwadmin_ro'@'%';
 FLUSH privileges;
 _EOF_
 
