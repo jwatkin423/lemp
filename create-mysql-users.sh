30bf330b63b8c9431f37180743c604f8b8e01780
Joseph Watkin <joseph.watkin@gmail.com>
2018-10-28 20:00:39 -0400

    removed databases setup

--- create-mysql-users.sh
+++ create-mysql-users.sh
@@ -21,26 +21,6 @@ CREATE USER 'jwadmin'@'%' IDENTIFIED BY ${PR_PASSWORD};
 CREATE USER 'jwadmin_dev'@'%' IDENTIFIED BY ${DEV_PASSWORD};
 CREATE USER 'jwadmin_qa'@'%' IDENTIFIED BY ${QA_PASSWORD};
 CREATE USER 'jwadmin_ro'@'%' IDENTIFIED BY ${RO_PASSWORD};
-DROP DATABASE IF EXISTS josephwatkin_pr;
-CREATE DATABASE josephwatkin_pr CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL ON josephwatkin_pr.* TO 'jwadmin'@'localhost';
-GRANT ALL ON josephwatkin_pr.* TO 'jwadmin'@'%';
-DROP DATABASE IF EXISTS josephwatkin_dev;
-CREATE DATABASE josephwatkin_dev CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL ON josephwatkin_dev.* TO 'jwadmin'@'localhost';
-GRANT ALL ON josephwatkin_dev.* TO 'jwadmin'@'%';
-GRANT ALL ON josephwatkin_dev.* TO 'jwadmin_dev'@'localhost';
-GRANT ALL ON josephwatkin_dev.* TO 'jwadmin_dev'@'%';
-GRANT SELECT, SHOW VIEW ON josephwatkin_dev.* TO 'jwadmin_ro'@'localhost';
-GRANT SELECT, SHOW VIEW ON josephwatkin_dev.* TO 'jwadmin_ro'@'%';
-DROP DATABASE IF EXISTS josephwatkin_qa;
-CREATE DATABASE josephwatkin_qa CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL ON josephwatkin_qa.* TO 'jwadmin'@'localhost';
-GRANT ALL ON josephwatkin_qa.* TO 'jwadmin'@'%';
-GRANT ALL ON josephwatkin_qa.* TO 'jwadmin_dev'@'localhost';
-GRANT ALL ON josephwatkin_qa.* TO 'jwadmin_dev'@'%';
-GRANT SELECT, SHOW VIEW ON josephwatkin_qa.* TO 'jwadmin_ro'@'localhost';
-GRANT SELECT, SHOW VIEW ON josephwatkin_qa.* TO 'jwadmin_ro'@'%';
 FLUSH privileges;
 _EOF_
 
